9ea79546e8022760cda487d0783acb913c4aff77
Aleksandr <103247048+pp8a@users.noreply.github.com>
2022-12-26 11:02:14 +0300

    Update CompositionRunner.java

--- OOP/src/composition/CompositionRunner.java
+++ OOP/src/composition/CompositionRunner.java
@@ -1,19 +1,8 @@
 package composition;
 
-
-import composition.buyer.Buyer;
-import composition.flower.DirectorFlowers;
-import composition.flower.Flower;
-import composition.flower.FlowerColor;
-import composition.flower.FlowerName;
-import composition.flower.SetFlowers;
 import composition.menu.Actions;
 import composition.menu.Menu;
-import composition.packaging.DirectorPacks;
-import composition.packaging.Pack;
-import composition.packaging.PackColor;
-import composition.packaging.PackType;
-import composition.packaging.SetPacks;
+
 
 public class CompositionRunner {
 
