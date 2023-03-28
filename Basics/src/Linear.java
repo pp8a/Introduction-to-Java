cc9e59a0638d844ce8fe3d933fe7ab31571bbaef
Aleksandr <103247048+pp8a@users.noreply.github.com>
2023-03-28 15:59:03 +0300

    Update Linear.java

--- Basics/src/Linear.java
+++ Basics/src/Linear.java
@@ -1,50 +1,7 @@
 import java.util.Scanner;
 
 public class Linear extends TaskSelection{
-	
-	public static int input() {		
-		int number;
-		@SuppressWarnings("resource")
-		Scanner in = new Scanner(System.in);	
 		
-		do {				
-		    while (!in.hasNextInt()) {
-		        System.err.println("It's not an integer number! ... Input:");
-		        in.next(); 
-		    }
-		    number = in.nextInt();
-		    if(number<=0) {
-		    	System.out.println("Input only positive number! ... Input:");
-		    }
-		} while (number <= 0);
-		
-		return number;
-	}
-	
-	public static int inputCoordinate() {		
-		int number;
-		@SuppressWarnings("resource")
-		Scanner in = new Scanner(System.in);
-		
-		while (!in.hasNextInt()) {
-				System.err.println("It's not an integer number! ... Input:");
-				in.next(); 
-		    }
-		number = in.nextInt();
-		
-		return number;
-	}
-	
-	public static double scale(double point) {
-		/*
-		 * количество знаков после запятой
-		 */
-		double scale = Math.pow(10, 3);
-		double result = Math.ceil(point * scale) / scale;
-		
-		return result;
-	}
-	
 	@Override	
 	public void taskOne() {
 		/*
@@ -169,5 +126,48 @@ public class Linear extends TaskSelection{
 		}else {
 			System.out.println(false);
 		}		
+	}
+	
+	public static int input() {		
+		int number;
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);	
+		
+		do {				
+		    while (!in.hasNextInt()) {
+		        System.err.println("It's not an integer number! ... Input:");
+		        in.next(); 
+		    }
+		    number = in.nextInt();
+		    if(number<=0) {
+		    	System.out.println("Input only positive number! ... Input:");
+		    }
+		} while (number <= 0);
+		
+		return number;
+	}
+	
+	public static int inputCoordinate() {		
+		int number;
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		
+		while (!in.hasNextInt()) {
+				System.err.println("It's not an integer number! ... Input:");
+				in.next(); 
+		    }
+		number = in.nextInt();
+		
+		return number;
+	}
+	
+	public static double scale(double point) {
+		/*
+		 * количество знаков после запятой
+		 */
+		double scale = Math.pow(10, 3);
+		double result = Math.ceil(point * scale) / scale;
+		
+		return result;
 	}
 }
