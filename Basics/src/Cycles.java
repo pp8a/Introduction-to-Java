aa7fc7a37be4e509f494c96ff009a7ff6abed695
Aleksandr <103247048+pp8a@users.noreply.github.com>
2023-03-28 16:00:58 +0300

    Update Cycles.java

--- Basics/src/Cycles.java
+++ Basics/src/Cycles.java
@@ -1,38 +1,6 @@
 import java.util.Scanner;
 
-public class Cycles extends TaskSelection{
-	
-	public static int input() {		
-		int number;
-		@SuppressWarnings("resource")
-		Scanner in = new Scanner(System.in);	
-		
-		do {				
-		    while (!in.hasNextInt()) {
-		        System.err.println("It's not an integer number! ... Input:");
-		        in.next(); 
-		    }
-		    number = in.nextInt();
-		    if(number<=0) {
-		    	System.out.println("Input only positive number! ... Input:");
-		    }
-		} while (number<=0);
-		
-		return number;
-	}
-	
-	public static double inputDouble() {		
-		
-		@SuppressWarnings("resource")
-		Scanner in = new Scanner(System.in);	
-						
-		while (!in.hasNextDouble()) {
-			System.err.println("It's not an double number! ... Input:");
-			in.next(); 
-		}
-		
-		return in.nextDouble();
-	}
+public class Cycles extends TaskSelection{	
 	
 	@Override	
 	public void taskOne() {
@@ -223,5 +191,36 @@ public class Cycles extends TaskSelection{
 		
 		return num;
 	}
-
+	
+	public static int input() {		
+		int number;
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);	
+		
+		do {				
+		    while (!in.hasNextInt()) {
+		        System.err.println("It's not an integer number! ... Input:");
+		        in.next(); 
+		    }
+		    number = in.nextInt();
+		    if(number<=0) {
+		    	System.out.println("Input only positive number! ... Input:");
+		    }
+		} while (number<=0);
+		
+		return number;
+	}
+	
+	public static double inputDouble() {		
+		
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);	
+						
+		while (!in.hasNextDouble()) {
+			System.err.println("It's not an double number! ... Input:");
+			in.next(); 
+		}
+		
+		return in.nextDouble();
+	}
 }
